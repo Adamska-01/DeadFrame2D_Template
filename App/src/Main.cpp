8027e6661f82c974fe4d47128cc027d4d1a7765e
Adamska-01 <silverworld@hotmail.it>
2025-10-07 00:18:01 +0200

    Refactored main

--- App/src/Main.cpp
+++ App/src/Main.cpp
@@ -1,16 +1,12 @@
 #include "Application.h"
-#include <memory>
 #include <Tools/FileSystem/PathMountResolver.h>
 
 
 int main(int argc, char** argv)
 {
-	// Do NOT remove this, unless you found a better solution
-	PathMountResolver::SetMountPrefix("Core");
+	Shared::Tools::PathMountResolver::SetMountPrefix("Core");
 
-	auto App = std::make_unique<Application>();
+	Application().Run();
 
-	App->Run();
-	
 	return 0;
 }
